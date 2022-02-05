e4e6eb3dccfc056ca9447d6ef1931ea09cc9703d
Liudmila <l0711@yandex.ru>
2022-02-05 19:01:22 +0300

    add page 69

---
+++ Learning/Page_69/pass_fail.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+
+	fmt.Print("enter a grade: ")
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	log.Fatal(err)
+	fmt.Println(input)
+
+	if grade == 100 {
+		fmt.Println("Perfect!")
+	} else if grade >= 60 {
+		fmt.Println("You pass.")
+	} else {
+		fmt.Println("You fail!")
+	}
+}
