b544b5dddb75bf2a6e7f6baada1fa5fb581ef73f
Liudmila Medova <liudmilailina00@gmail.com>
2022-02-05 22:46:54 +0300

    Add err

--- Learning/Page_69/pass_fail.go
+++ Learning/Page_69/pass_fail.go
@@ -12,14 +12,9 @@ func main() {
 	fmt.Print("enter a grade: ")
 	reader := bufio.NewReader(os.Stdin)
 	input, err := reader.ReadString('\n')
-	log.Fatal(err)
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(input)
 
-	if grade == 100 {
-		fmt.Println("Perfect!")
-	} else if grade >= 60 {
-		fmt.Println("You pass.")
-	} else {
-		fmt.Println("You fail!")
-	}
 }
