c2dddbfe72623477d8103c0223b58c957e8f3b4d
Liudmila Medova <liudmilailina00@gmail.com>
2022-03-15 00:16:25 +0300

    Finished programm list 69

--- Learning/Page_69/pass_fail.go
+++ Learning/Page_69/pass_fail.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log" // для вывода ошибки
 	"os"  // для чтения с клавиатуры
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -12,9 +14,23 @@ func main() {
 	fmt.Print("enter a grade: ")
 	reader := bufio.NewReader(os.Stdin)
 	input, err := reader.ReadString('\n')
+
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(input)
 
+	input = strings.TrimSpace(input)
+	grade, err := strconv.ParseFloat(input, 64)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var status string
+	if grade >= 60 {
+		status = "passing"
+	} else {
+		status = "failing"
+	}
+	fmt.Println("A grade of", grade, "is", status)
 }
