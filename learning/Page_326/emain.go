013318659d4349c3d607b2a235356d30bcb46cd6
Liudmila Medova <liudmilailina00@gmail.com>
2023-04-07 18:14:53 +0300

    Save 07.04.2023

---
+++ learning/Page_326/emain.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"learning/calendar"
+	"log"
+)
+
+func main() {
+	date := calendar.Date{}
+	err := date.SetYear(2019)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = date.SetMonth(12)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = date.SetDay(27)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(date.Year())
+	fmt.Println(date.Month())
+	fmt.Println(date.Day())
+}
